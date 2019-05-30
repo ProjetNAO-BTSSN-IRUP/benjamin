59901fb936f585be8674f9e386202485689d0756
ProjetNAO-BTSSN-IRUP <47860845+ProjetNAO-BTSSN-IRUP@users.noreply.github.com>
2019-05-30 12:19:11 +0200

    integration reconnaissance qr code + gestion des erreurs

--- audio_response.py
+++ audio_response.py
@@ -6,41 +6,16 @@ from naoqi import ALProxy
 import MySQLdb
 
 
-def nao_ip():
-    ip_nao = socket.gethostbyname("NAO")
-    return ip_nao
-
-
-def qr_code_recognition():
-    # ip_nao = nao_ip()
-    ip_nao = "192.168.0.25"
-    barcode = ALProxy("ALBarcodeReader", ip_nao, 9559)
-    barcode.subscribe("test_barcode")
-    
-    memory = ALProxy("ALMemory", ip_nao, 9559)
-    
-    for i in range(20):
-        data = memory.getData("BarcodeReader/BarcodeDetected")
-        if data:
-            is_recognized = True
-            data = data[0][0].split(",")
-            print type(data), data
-            time.sleep(1)
-            return is_recognized, data
-        
-
-qr_code_recognition()
-
 class Response:
     
     def __init__(self):
-        ip_nao = nao_ip()
-        print ">>>{0}".format(ip_nao)
-        self.tts = ALProxy("ALTextToSpeech", ip_nao, 9559)
+        self.ip_nao = socket.gethostbyname("NAO")
+        print ">>>{0}".format(self.ip_nao)
+        self.tts = ALProxy("ALTextToSpeech", self.ip_nao, 9559)
         
-        db = MySQLdb.connect(host="SRV-NAO",  # your host
-                             user="benjamin",  # username
-                             passwd="benjamin",  # password
+        db = MySQLdb.connect(host="127.0.0.1",  # your host
+                             user="root",  # username
+                             passwd="",  # password
                              db="benjamin",
                              charset="utf8")  # name of the database
         
@@ -48,9 +23,26 @@ class Response:
         self.cur = db.cursor()
         # Select data from table using SQL query.
         self.cur.execute("SELECT Reponse FROM audio")
-        self.prenom = "Frederic"
-        self.nom = "Chopin"
-        self.retard = 40
+
+    def qr_code_recognition(self):
+        barcode = ALProxy("ALBarcodeReader", self.ip_nao, 9559)
+        barcode.subscribe("test_barcode")
+    
+        memory = ALProxy("ALMemory", self.ip_nao, 9559)
+        
+        is_recognized = False
+        
+        for i in range(20):
+            data = memory.getData("BarcodeReader/BarcodeDetected")
+            if data:
+                is_recognized = True
+                data = data[0][0].split(",")
+                print type(data), data
+                time.sleep(1)
+                return is_recognized, data
+            
+            else:
+                return is_recognized, data
     
     def get_response_in_database(self):
         n = 0
@@ -73,14 +65,24 @@ class Response:
             print "Aucune reconnaissance"
 
     def response_from_qr_code_recognition(self):
-        is_recognized = True
-    
-        if is_recognized:
+        for i in range(3):
+            # 3 tentatives de reconnaissance de QR code
+            print i
+            is_recognized, data = self.qr_code_recognition()
+            if is_recognized:
+                self.get_response_in_database()
+                break  # On sort de la boucle si un qr code est reconnu
+                
+            print is_recognized
+                
+            time.sleep(2)
             
-            self.get_response_in_database()
+            # Si aucun qr code n'est reconnu au bout des 3 essais 
+            if i == 2 and is_recognized == False:
+                self.tts.say("Pas de QR code reconnu.")
     
-        else:
-            print "Aucune reconnaissance"
+        
+    
             
 
-# Response().response_from_qr_code_recognition()
+Response().response_from_qr_code_recognition()
